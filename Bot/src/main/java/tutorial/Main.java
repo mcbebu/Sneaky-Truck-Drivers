68675e3cdd0c6d3262b1505cfa3b7033456a0371
Elijah Chia <chia.elijah@gmail.com>
2023-02-25 17:14:11 +0800

    Update Main.java

--- Bot/src/main/java/tutorial/Main.java
+++ Bot/src/main/java/tutorial/Main.java
@@ -1,13 +1,18 @@
 package tutorial;
-
+import java.util.HashMap;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
+        Day day = new Day(10); // Assume only 10 parcels per slot
+        HashMap<String, Integer> options = day.getOptions();
+        int optionsSize = options.size();
+        String[] timingOptions = options.keySet().toArray(new String[optionsSize]);
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         Bot bot = new Bot();
+        bot.addTimings(timingOptions);
         botsApi.registerBot(new Bot());
 
     }
