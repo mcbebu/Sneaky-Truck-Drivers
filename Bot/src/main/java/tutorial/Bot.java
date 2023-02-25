2dffa44cd9c15fe37d412bf92edd5e5455f6d325
Elijah Chia <chia.elijah@gmail.com>
2023-02-25 21:40:05 +0800

    Update Bot.java

--- Bot/src/main/java/tutorial/Bot.java
+++ Bot/src/main/java/tutorial/Bot.java
@@ -4,6 +4,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -13,6 +14,8 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -30,6 +33,16 @@ public class Bot extends TelegramLongPollingBot {
         return REDACTED;
     }
 
+    private String[] timings = new String[0];
+    int timingSize;
+    //the timings are initialised when the bot is created
+    public void addTimings(String[] timings) {
+        this.timings = timings;
+        timingSize = timings.length;
+    }
+
+    //date time format
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLL yyyy");
     @Override
     public void onUpdateReceived(Update update) {
         // var msg = update.getMessage();
@@ -53,20 +66,40 @@ public class Bot extends TelegramLongPollingBot {
             String chat_id = update.getCallbackQuery().getMessage().getChatId().toString();
 
             if (call_data.equals("Yes")) {
-                EditMessageText new_message = new EditMessageText();
-                new_message.setChatId(chat_id);
-                new_message.setMessageId((int) message_id);
-                new_message.setText("FINALLY WORKING");
+                DeleteMessage deleteMsg = new DeleteMessage(chat_id, (int) message_id);
+                //if consignee chooses yes, show them all the available timeslots
+
                 try {
-                    execute(new_message);
+                    execute(deleteMsg);
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
+                //display buttons
+                List<List<InlineKeyboardButton>> someButtons = new ArrayList<List<InlineKeyboardButton>>();
+                for (int i = 0; i < timingSize; i++) {
+                    //display each button
+                    var builtButton = InlineKeyboardButton.builder().text(timings[i]).callbackData(timings[i]).build();
+                    someButtons.add(List.of(builtButton));
+                }
+                InlineKeyboardMarkup keyboard2 = new InlineKeyboardMarkup(someButtons);
+
+                Long wrappedUserId = userId;
+                SendMessage sm = SendMessage.builder().chatId(wrappedUserId.toString())
+                        .parseMode("HTML").text("Please select your preferred timeslot:")
+                        .replyMarkup(keyboard2).build();
+                try {
+                    execute(sm);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+
             } else if (call_data.equals("No")) {
                 EditMessageText new_message = new EditMessageText();
                 new_message.setChatId(chat_id);
                 new_message.setMessageId((int) message_id);
-                new_message.setText("FINALLY WORKING");
+                LocalDate localDate = LocalDate.now().plusDays(2);
+                //returns the date in 2 days in the format eg 25 Feb 2023
+                new_message.setText("Your parcel will arrive on " + localDate.format(formatter));
                 try {
                     execute(new_message);
                 } catch (TelegramApiException e) {
@@ -89,11 +122,6 @@ public class Bot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
-    }
-    private String[] timings;
-    //the timings are initialised when the bot is created
-    public void addTimings(String[] timings) {
-        this.timings = timings;
     }
 
     public void timingVote(Long userId){
