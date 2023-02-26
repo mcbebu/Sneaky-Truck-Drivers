f1461694522959c05721927f4350223e3c2131a3
Elijah Chia <chia.elijah@gmail.com>
2023-02-26 10:50:45 +0800

    Update Main.java

--- Bot/src/main/java/tutorial/Main.java
+++ Bot/src/main/java/tutorial/Main.java
@@ -1,15 +1,14 @@
-package tutorial;
-
+import org.apache.log4j.BasicConfigurator;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
+        //to stop slf4j warnings
+        BasicConfigurator.configure();
         Day day = new Day(10); // Assume only 10 parcels per slot
         ArrayList<String> options = day.getOptions(); //retrieve the current options
         int optionsSize = options.size();
